03b1d42e24abcdfa9440c5091009298aa99208a8
e-nagornyak <dolkiv@gmail.com>
2022-10-09 20:47:49 +0300

    Added components Header and Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Technologies from "./Technologies";
+import Header from "./Header";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
 export default App;
