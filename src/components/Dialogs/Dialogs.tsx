9b2de931844698977460446f4c6482f382876fd9
e-nagornyak <dolkiv@gmail.com>
2022-10-23 22:30:37 +0300

    Added Dialogs (Lessons 21)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,8 +5,18 @@ type DialogsType = {}
 
 export const Dialogs = (props: DialogsType) => {
     return (
-        <div>
-            <h1>It`s dialogs component</h1>
+        <div className={s.dialogs}>
+            <div className={s.items}>
+                <div className={`${s.item} ${s.active}`}>Jenia</div>
+                <div className={s.item}>Lisa</div>
+                <div className={s.item}>Vasia</div>
+                <div className={s.item}>Bogdan</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your day?</div>
+                <div className={s.message}>Yo!</div>
+            </div>
         </div>
     );
 };
