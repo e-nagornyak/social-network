9fbcc9ee0182c11a2f02dd14b66e5894bdb91043
e-nagornyak <dolkiv@gmail.com>
2022-10-26 23:27:56 +0300

    Refactored Dialogs and MyPosts (Lessons 24)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,12 @@ import {Post} from "./Post/Post";
 import s from './MyPosts.module.css'
 
 export const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: 'How are you?', likesCount: 23},
+        {id: 2, message: 'Hello!', likesCount: 12}
+    ]
+
     return (
         <div>
             <h1>my post</h1>
@@ -16,8 +22,8 @@ export const MyPosts = () => {
 
             </div>
             <div className={s.posts}>
-                <Post message={'Hi, how are you?'} likesCount={23}/>
-                <Post message={'It`s my first post?'} likesCount={0}/>
+                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
             </div>
         </div>
     );
