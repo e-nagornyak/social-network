4bf8e03c51b58f503e1fe68e0b0bbe221813d2a3
e-nagornyak <dolkiv@gmail.com>
2022-11-26 21:29:24 +0200

    Redux (lesson 41)

--- src/redux/reducers/dialogsReducer.ts
+++ src/redux/reducers/dialogsReducer.ts
@@ -1,8 +1,28 @@
 import {v1} from "uuid";
-import {DialogsPageType, MessagesType} from "../state";
+import {DialogsPageType, MessagesType} from "../store";
+
+const initialState = {
+    messages: [
+        {id: v1(), message: 'Hello'},
+        {id: v1(), message: 'How are you?'},
+        {id: v1(), message: 'How'},
+        {id: v1(), message: 'Yo!?'},
+        {id: v1(), message: 'How are you?'},
+        {id: v1(), message: 'Yes i am'}
+    ],
+    dialogs: [
+        {id: v1(), name: 'Evhen'},
+        {id: v1(), name: 'Lisa'},
+        {id: v1(), name: 'Bogdan'},
+        {id: v1(), name: 'Sonia'},
+        {id: v1(), name: 'Ivan'},
+        {id: v1(), name: 'Sasha'}
+    ],
+    newMessageText: '',
+}
 
 export type DialogsActionType = sendMessageACType | updateMessageTextACType
-export const dialogsReducer = (state: DialogsPageType, action: DialogsActionType) => {
+export const dialogsReducer = (state: DialogsPageType = initialState, action: DialogsActionType) => {
     switch (action.type) {
         case "SEND-MESSAGE": {
             const newMessage: MessagesType = {id: v1(), message: state.newMessageText}
