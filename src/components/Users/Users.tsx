f1fea9a9f3588ffc3e76c0ddabec3b561efe7dbf
e-nagornyak <dolkiv@gmail.com>
2022-12-09 01:39:35 +0200

    Side Effect (lesson 52)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,16 +3,21 @@ import {UsersPropsType} from "./UsersContainer";
 import s from './users.module.css'
 import axios from "axios";
 import userDefault from '../../assets/img/default_avatar.png'
-export const Users: React.FC<UsersPropsType> = (props) => {
 
-    if (props.usersPage.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+export const Users: React.FC<UsersPropsType> = (props) => {
+    const getUsers = () => {
+        if (props.usersPage.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
+
+
     return (
         <div className={s.content}>
+            <button onClick={getUsers}>Get Users</button>
             {props.usersPage.users.map(u => {
 
                 const FollowHandler = () => props.follow(u.id)
