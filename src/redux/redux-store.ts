bd7fae5d70894011288bc4e4a7bf76e83105d405
e-nagornyak <dolkiv@gmail.com>
2023-01-11 23:20:34 +0200

    Created types AppDispatch, AppThunk

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,9 +1,9 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {profileReducer} from "./reducers/profileReducer";
-import {dialogsReducer} from "./reducers/dialogsReducer";
-import {usersReducer} from "./reducers/usersReducer";
-import {authReducer} from "./reducers/authReducer";
-import thunkMiddleware from "redux-thunk";
+import {AnyAction, applyMiddleware, combineReducers, createStore} from "redux";
+import {profileReducer} from "./reducers/profile-reducer";
+import {dialogsReducer} from "./reducers/dialogs-reducer";
+import {usersReducer} from "./reducers/users-reducer";
+import {authReducer} from "./reducers/auth-reducer";
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {reducer as formReducer} from 'redux-form'
 
 
@@ -13,10 +13,13 @@ export const rootReducer = combineReducers({
     usersPage: usersReducer,
     auth: authReducer,
     form: formReducer
-
 });
 
 export type AppStateType = ReturnType<typeof rootReducer>
+// export type AppDispatch = typeof store.dispatch
+export type AppDispatch = ThunkDispatch<AppStateType, unknown, AnyAction>
+// ReturnType - те, що повертає санка
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AnyAction>
 
 export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
