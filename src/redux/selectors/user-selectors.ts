e62f6cfc66047e962faeca109e9485c588ae88c0
e-nagornyak <dolkiv@gmail.com>
2023-01-22 12:46:48 +0200

    Selectors (reselect part 2)

--- src/redux/selectors/user-selectors.ts
+++ src/redux/selectors/user-selectors.ts
@@ -6,3 +6,4 @@ export const getTotalUsersCount = (state: AppStateType) =>  state.usersPage.tota
 export const getCurrentPage = (state: AppStateType) =>  state.usersPage.currentPage
 export const getIsFetching = (state: AppStateType) =>  state.usersPage.isFetching
 export const getFollowingProgress = (state: AppStateType) =>  state.usersPage.followingInProgress
+// export const getFollowingProgress = (state: AppStateType) =>  state.usersPage.followingInProgress
